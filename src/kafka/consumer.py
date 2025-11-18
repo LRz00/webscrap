c268e59a6a40e43c5a56b355975e633d0f1cd127
Lara Rodrigues <lararods73@gmail.com>
2025-11-18 14:22:21 -0300

    feat: add first version of consumer with no price variation check

--- src/kafka/consumer.py
+++ src/kafka/consumer.py
@@ -1,58 +1,20 @@
-import redis
 from kafka import KafkaConsumer
 import json
-from src.configs.config import config
-def connect_redis():
-    return redis.Redis(
-        host=config["redis_host"],
-        port=config["redis_port"],
-        decode_responses=True
-    )
+from src.notifier.discord_notifier_bot import DiscordNotifier
 
+consumer = KafkaConsumer(
+    "wishlist_prices",
+    bootstrap_servers="localhost:9092",
+    value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+)
 
-def create_consumer():
-    return KafkaConsumer(
-        config["kafka_topic"],
-        bootstrap_servers=config["kafka_broker"],
-        value_deserialize=lambda v: json.loads(v.decode("utf-8")),
-        auto_offset_reset="earliest",
-        enable_auto_commit=True,
-        group_id="wishlist-alerts"
-    )
+notifier = DiscordNotifier()
 
+for message in consumer:
+    items = message.value
 
-def check_variation(item_name, new_price, r):
-    last_price = r.get(item_name);
+    msg = "**Wishlist atualizada:**\n"
+    for item in items:
+        msg += f"- {item['name']}: {item['price']}\n"
 
-    try:
-        new_price = float(str(new_price).replace(",", "."))
-    except ValueError:
-        print(f"Invalid price")
-        return
-    
-
-    if last_price:
-        last_price = float(last_price)
-        variation = ((last_price - new_price) / last_price) * 100
-
-        if variation >= 5:
-            #send alert
-            print(f"alert")
-    
-    r.set(item_name, new_price)
-
-
-def consume_messages():
-    print(f"Consumer started listening to prices")
-    consumer = create_consumer()
-    r = connect_redis()
-
-    for msg in consumer:
-        item = msg.value
-        name = item.get("name")
-        price = item.get("price")
-
-        if not name or not price or price == "N/A" or name == "N/A":
-            continue
-        
-        check_variation(name, price, r)
+    notifier.send_message(msg)
